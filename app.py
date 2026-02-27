f4b7284e5ae5346ba0bd054781758c481d04ce21
maerd47 <78052898+maerd47@users.noreply.github.com>
2026-02-26 22:22:14 -0500

    'updated'

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ def predict():
     print(final_input)
     output=regmodel.predict(final_input)[0]
     print(output)
+    res=app.test_client().get("/")
+    print(res.status_code)
+    
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
